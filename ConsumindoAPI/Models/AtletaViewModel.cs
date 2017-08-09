3243c78cbf0f4802312082ade4092921f7b16cf4
bruno.silva <bruno.silva@061N5009091928>
2017-08-09 13:21:28 -0300

    Acertando a viewModel e atualizando CartolaJson

--- ConsumindoAPI/Models/AtletaViewModel.cs
+++ ConsumindoAPI/Models/AtletaViewModel.cs
@@ -10,38 +10,48 @@ namespace ConsumindoAPI.Models
         public int atleta_id { get; set; }
 
         public string nome { get; set; }
+
         [DisplayName("Jogador")]
         public string apelido { get; set; }
+
         [DisplayName("Clube")]
         public string nome_clube { get; set; }
+
         public int posicao_id { get; set; }
+
         public int status_id { get; set; }
+
         [DisplayName("Jogos")]
         public int jogos_num { get; set; }
+
         [DisplayName("Preço")]
         public double preco_num { get; set; }
+
         [DisplayName("Status")]
         public string Status { get; set; }
+
         //Média
         [DisplayName("Média")]
-        [DisplayFormat(DataFormatString = "{0:#,##0.0#}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:#,##0.00}", ApplyFormatInEditMode = true)]
         public double media { get; set; }
 
         //Média
         [DisplayName("Média Gols Sofridos")]
-        [DisplayFormat(DataFormatString = "{0:#,##0.0#}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:#,##0.00}", ApplyFormatInEditMode = true)]
         public double mediaGolsSofridos { get; set; }
 
         [DisplayName("Média Finalizações")]
-        [DisplayFormat(DataFormatString = "{0:#,##0.0#}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:#,##0.00}", ApplyFormatInEditMode = true)]
         public double finalizacoes { get; set; }
+
         [DisplayName("Média DD")]
-        [DisplayFormat(DataFormatString = "{0:#,##0.0#}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:#,##0.00}", ApplyFormatInEditMode = true)]
         public double mediaDefesasDificeis { get; set; }
 
         [DisplayName("Pontos Média DD")]
-        [DisplayFormat(DataFormatString = "{0:#,##0.0#}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:#,##0.00}", ApplyFormatInEditMode = true)]
         public double pontuacaoMediaDD { get; set; }
+
         [DisplayFormat(DataFormatString = "{0:#,##0.00}", ApplyFormatInEditMode = true)]
         [DisplayName("Total Pontos")]
         public double totalPontos { get; set; }
--- ConsumindoAPI/Models/PartidaViewModel.cs
+++ ConsumindoAPI/Models/PartidaViewModel.cs
@@ -7,13 +7,17 @@ namespace ConsumindoAPI.Models
     {
         [Key]
         public int PartidaId { get; set; }
+
         [DisplayName("Mandante")]
         public string clube_casa { get; set; }
+
         [DisplayName("Visitante")]
         public string clube_visitante { get; set; }
+
         [DisplayName("Gols")]
         [DisplayFormat(DataFormatString = "{0:#,##0.0#}", ApplyFormatInEditMode = true)]
         public double clube_casa_gols { get; set; }
+
         [DisplayName("Gols")]
         [DisplayFormat(DataFormatString = "{0:#,##0.0#}", ApplyFormatInEditMode = true)]
         public double clube_visitante_gols { get; set; }
